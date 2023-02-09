f682d72ce409c4fcb1253dc141bed4250981db29
LionX-S <13141096848@163.com>
2023-02-09 11:37:02 +0800

    feat：watch与watchEffect函数

---
+++ 05_src_watch与watchEffect函数/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<Demo/>
+</template>
+
+<script>
+	import Demo from "./components/Demo.vue";
+	export default {
+		name: "App",
+		components: { Demo },
+		setup() {
+			function helloFun(value) {
+				console.log("触发了自定义事件，收到的值为" + value);
+			}
+
+			return {
+				helloFun
+			}
+		}
+	};
+</script>
+
+<style></style>
